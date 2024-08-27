f5774987be2d2e7bb50bb7c31536fee7a3659e5a
Leotrim <leootrim221@gmail.com>
2024-08-27 22:58:31 +0200

    SideBar UI

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import Login from './pages/login/Login';
+import SideBar from './components/Sidebar/SideBar';
+import Dashboard from './pages/dashboard/Dashboard';
 import './App.css';
+import { useLayoutEffect } from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Login />
-      </header>
-    </div>
+    <Router>
+      
+          <Routes>
+            <Route path="/" element={<Login />} /> 
+            <Route
+              path="/admin/*"
+              element={
+                <div className='main-layout'>
+                  <SideBar />
+                  <div className='content'>
+                    <Routes>
+                      <Route path='/dashboard' element={<Dashboard />} />
+                    </Routes>
+                  </div>
+                </div>
+              }
+              />
+          </Routes>
+    </Router>
   );
 }
 
