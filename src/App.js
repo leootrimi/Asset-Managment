3d4adfa26730f1177ff10f7a612f91bf4b6f9916
Leotrim <leootrim221@gmail.com>
2024-08-30 16:15:51 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import Login from './pages/login/Login';
+import Navbar from './components/navbar/Navbar';
 import SideBar from './components/Sidebar/SideBar';
 import Dashboard from './pages/dashboard/Dashboard';
+import AddUser from './pages/addUser/AddUser';
+import ShowUsers from './pages/showUsers/ShowUsers';
+import ShowEquipment from './pages/showEquipment/ShowEquipment';
 import './App.css';
 import { useLayoutEffect } from 'react';
 
@@ -17,9 +21,12 @@ function App() {
               element={
                 <div className='main-layout'>
                   <SideBar />
-                  <div className='content'>
+                  <div className='content '>
                     <Routes>
                       <Route path='/dashboard' element={<Dashboard />} />
+                      <Route path='/add/user' element={<AddUser />} />
+                      <Route path='/show/user' element={<ShowUsers />} />
+                      <Route path='/show/equipment' element={<ShowEquipment />} />
                     </Routes>
                   </div>
                 </div>
