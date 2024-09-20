bb9c50412200da000ca4f8ec2000614cc2657852
Leotrim <leootrim221@gmail.com>
2024-09-20 13:34:31 +0200

    Add UI and function to save a model in database

--- FrontEnd/src/pages/addEquipment/AddEquipment.jsx
+++ FrontEnd/src/pages/addEquipment/AddEquipment.jsx
@@ -1,13 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import { BiReset } from 'react-icons/bi';
-import { fetchModel, fetchModelCount } from '../../services/AddEquipment';
+import { fetchModel, fetchModelCount, submitModel, getCount } from '../../services/AddEquipment';
 import Navbar from '../../components/navbar/Navbar';
 
 function AddEquipment() {
   const [employers, setEmployers] = useState([]);
   const [selectedEmployer, setSelectedEmployer] = useState('');
-  const count = '0000';
   const [models, setModels] = useState([]); 
+
+  const [location, setLocation] = useState('');
+  const [company, setCompany] = useState('');
+  const [type, setType] = useState('');
+  const [model, setModel] = useState('');
+
+  
+
   const [formData, setFormData] = useState({
     type: '',
     model: '',
@@ -20,6 +27,7 @@ function AddEquipment() {
     dateOfReceipt: '',
     warrantyExpirationDate: '',
     supplier: '',
+    departament: '',
   });
 
   useEffect(() => {
@@ -64,22 +72,51 @@ function AddEquipment() {
     setSelectedEmployer(e.target.value);
   };
 
+
+  const handleSubmit1 = async (e) => {
+    e.preventDefault();
+  
+  
+    try {
+
+      var count = await getCount();
+      console.log(count);
+    
+
+      const paddedModelCount = String((count * 10) + 10).padStart(3, '0');
+      const prefix = company + "-" + location + "-" + paddedModelCount + "-"
+      const modelData = { type, model, prefix };
+      console.log(modelData);
+
+
+      const response = await submitModel(modelData);
+      console.log(response); 
+  
+      alert('Data submitted successfully!');
+    } catch (error) {
+      console.error('Error submitting data:', error);
+      alert('Failed to submit data. Please try again.');
+    }
+
+  };
+
+
   const handleChange = async (e) => {
     const { id, value } = e.target;
-  
     if (id === 'type') {
       setFormData((prevData) => ({
         ...prevData,
         [id]: value,
         model: '', 
-        tag: '',  
+        tag: '',
+        
       }));
       fetchModelsByType(value);
     } else if (id === 'model') {
       const selectedModel = models.find((model) => model.model === value);
       
       const modelCount = await fetchModelCount(value);
-      const paddedModelCount = String(modelCount).padStart(4, '0');
+      const paddedModelCount = String(modelCount + 1).padStart(4, '0');
       console.log(modelCount);
       
       setFormData((prevData) => ({
@@ -123,7 +160,7 @@ function AddEquipment() {
         },
         body: JSON.stringify(dataToSubmit),
       });
-
+      console.log(formData);
       if (!response.ok) {
         const errorText = await response.text();
         throw new Error(`HTTP error! status: ${response.status}`);
@@ -143,6 +180,7 @@ function AddEquipment() {
         dateOfReceipt: '',
         warrantyExpirationDate: '',
         supplier: '',
+        departament: '',
       });
       setSelectedEmployer('');
     } catch (error) {
@@ -324,7 +362,7 @@ function AddEquipment() {
               <select
                 className="form-control"
                 id="departament"
-                value={formData.assignedForm}
+                value={formData.departament}
                 onChange={handleChange}
               >
                 <option value="91Life">91Life</option>
@@ -343,6 +381,90 @@ function AddEquipment() {
           <BiReset className="mr-1" />
           Reset
         </button>
+
+  <button type="button" className="btn1 p-2" data-bs-toggle="modal" data-bs-target="#exampleModal">
+    Add Model
+  </button>
+
+        <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div className="modal-body">
+      <form id="deviceForm" onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="location">Location</label>
+          <select
+            id="location"
+            className="form-control"
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+          >
+            <option value="" disabled>Select Location</option>
+            <option value="PR">Prishtine</option>
+            <option value="MT">Tirane</option>
+          </select>
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="company">Company</label>
+          <select
+            id="company"
+            className="form-control"
+            value={company}
+            onChange={(e) => setCompany(e.target.value)}
+          >
+            <option value="" disabled>Select Company</option>
+            <option value="91">91Life</option>
+            <option value="MT">Matrics 2</option>
+          </select>
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="type">Type</label>
+          <select
+            id="type"
+            className="form-control"
+            value={type}
+            onChange={(e) => setType(e.target.value)}
+          >
+            <option value="" disabled>Select Type</option>
+            <option value="Laptop">Laptop</option>
+            <option value="Desktop">Desktop</option>
+            <option value="Mouse">Mouse</option>
+            <option value="TV">TV</option>
+            <option value="Printer">Printer</option>
+            <option value="Headphones">Headphones</option>
+          </select>
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="model1">Model</label>
+          <input
+            type="text"
+            id="model1"
+            className="form-control"
+            placeholder="Enter Model"
+            value={model}
+            onChange={(e) => setModel(e.target.value)}
+          />
+        </div>
+
+       
+      </form>
+    </div>
+
+              <div className="modal-footer">
+                <button type="button" className="btn1 p-2" data-bs-dismiss="modal">Close</button>
+                <button type="submit" className="btn1 p-2" onClick={handleSubmit1}>Submit</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
       </div>
     </div>
   );
--- FrontEnd/src/services/AddEquipment.js
+++ FrontEnd/src/services/AddEquipment.js
@@ -36,6 +36,45 @@ export const fetchEmployers = async () => {
       throw error; 
     }
   };
+
+  export const submitModel = async (data) => {
+    try {
+      const response = await fetch('http://127.0.0.1:8000/model/add/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (!response.ok) {
+        const errorText = await response.text();
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+  
+      const result = await response.json();
+      return result; 
+    } catch (error) {
+      console.error('Error submitting equipment data:', error.message);
+      throw error; 
+    }
+
+  }
+
+  export const getCount = async() => {
+    try {
+      const response = await fetch(`http://127.0.0.1:8000/model/get/count/`);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+  
+      const data = await response.json();
+      return data; 
+    } catch (error) {
+      console.error('Error fetching model data:', error.message);
+      throw error;
+    }
+  }
   
   export const fetchModel = async (type) => {
     try {
@@ -55,7 +94,7 @@ export const fetchEmployers = async () => {
 
   export const fetchModelCount = async (model) => {
     try {
-      const response = await fetch(`http://127.0.0.1:8000/model/get/${model}/count`);
+      const response = await fetch(`http://127.0.0.1:8000/equipment/count/${model}/`);
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
       }
